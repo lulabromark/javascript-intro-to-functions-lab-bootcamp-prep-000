1b84033f5611ea33648782da227fac3695a39735
Mark Lawrence <lawrence.ml@gmail.com>
2018-04-23 22:05:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = "hello!"
-  if (string.toLowerCase() === lowercase) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
-  }
+  } else if {
 }
